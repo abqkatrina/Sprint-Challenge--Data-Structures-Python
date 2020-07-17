14650c946caffff7a3a4bce311091c17577efc4d
Katrina Hernandez <abqkatrina@gmail.com>
2020-07-17 15:12:05 -0600

    replaced name list with name tree

--- names/names.py
+++ names/names.py
@@ -11,7 +11,7 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-duplicates = [ x for x in names_1 if x in names_2 ]
+# duplicates = [ x for x in names_1 if x in names_2 ]
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
@@ -20,6 +20,45 @@ duplicates = [ x for x in names_1 if x in names_2 ]
 #             duplicates.append(name_1)
 
 # *You may not use the built in Python list, set, or dictionary in your solution for this problem.  However, you can and should use the provided `duplicates` list to return your solution.*
+class Node:
+    def __init__(self, value):
+        self.value = value #name
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = Node(value)
+        if value >= self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = Node(value)
+    
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        elif target > self.value:
+            if not self.right:
+                return False
+            return self.right.contains(target)
+
+tree = Node(names_1[0])
+for n in names_1:
+    tree.insert(n)
+for n in names_2:
+    if tree.contains(n):
+        duplicates.append(n)
+
+        
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
